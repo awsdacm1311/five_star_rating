cb27b48bb0a6449527ed93c2530fc3cfd45217fb
DanielC <dacm1311@hotmail.com>
2022-08-09 21:11:19 -0500

    my rating star app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {AiOutlineStar } from 'react-icons/ai'
+import React, { useState } from 'react';
+
+function CreateArray(number=5){
+  return [...Array(number)];
+}
+
+function Star({key, setClick, color }){
+  return(
+    <AiOutlineStar fill={color} onClick={setClick}/>
+  )
+}
+
+function StarRating({starNumber}){
+
+  let [click, setClick] = useState(0);
+  let array = CreateArray(starNumber);
+
+  return(
+    <div>
+    
+      {
+        array.map((n, i) => 
+         
+         <Star key={i} setClick={() => setClick(i+1)} color={ i<click?'red':'gray'}/>
+        )
+      }
+      
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StarRating starNumber={10}/>
+    
     </div>
   );
 }
